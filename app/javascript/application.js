a2029fa932807aeb1037d2c35210dc94ca60c3a9
goosemeister <jacobus@euca.co.za>
2024-11-18 15:14:07 +0200

    Fixes on setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
+//= require rails-ujs
+//= require_tree .
